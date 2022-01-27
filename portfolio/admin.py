a4aba19b9892636c4e3a7d6bbc37cad112732228
thoth <thoth2357@gmail.com>
2022-01-27 08:32:16 -0500

    set up admin page for portfolio config

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from .models import *
+from django import forms
 
 # Register your models here.
+class User_info_Form(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['Fullname'].required = True
+
+    class Meta:
+        model = User_info
+        fields = '__all__'
 class User_info_inline(admin.StackedInline):
+    form  = User_info_Form
     model = User_info
 
 class Personalization_Inline(admin.TabularInline):
@@ -12,7 +22,7 @@ class Personalization_Inline(admin.TabularInline):
 class PortfolioAdmin(admin.ModelAdmin):
     inlines = [Personalization_Inline, User_info_inline]
 
-
+    readonly_fields = ['Portfolio_name']
 
 
 
--- portfolio/models.py
+++ portfolio/models.py
@@ -7,7 +7,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 class Portfolio(models.Model):
-    pass
+    Portfolio_name = models.CharField(_("portfolio_name/id"), max_length=50, null=True, unique=False, default='MY PORTFOLIO CONFIG')
     # User_info = models.OneToOneField("portfolio.User_info", verbose_name=_("user_info"), on_delete=models.CASCADE)
     # Personlization = models.OneToOneField("portfolio.Personlization", verbose_name=_("personalization"), on_delete=models.CASCADE)
 
@@ -15,10 +15,10 @@ class Portfolio(models.Model):
         return f'Portfolio'
 class User_info(models.Model):
     Portfolio = models.OneToOneField("portfolio.Portfolio", verbose_name=_("User_info"), on_delete=models.CASCADE, null=True)
-    Fullname = models.CharField(_("FullName"), max_length=50, help_text='Full name to be displayed on portfolio')
-    Country = CountryField(help_text="Country of Residence")
-    City = models.CharField(_('City_Name'), max_length=50, help_text="name of city that would be displayed on front page")
-    Phone_number = PhoneNumberField(_("Phone Number"), help_text="Phone number to display on home page")
+    Fullname = models.CharField(_("FullName"), max_length=50, help_text='Full name to be displayed on portfolio', default='Enter Full Name')
+    Country = CountryField(help_text="Country of Residence", default='Choose Country')
+    City = models.CharField(_('City_Name'), max_length=50, help_text="name of city that would be displayed on front page", default='Enter City Name')
+    Phone_number = PhoneNumberField(_("Phone Number"), help_text="Phone number to display on home page", default='Enter phone number')
     class Meta:
         verbose_name = ("User_Information")
         verbose_name_plural = ("User_Information")
@@ -27,7 +27,7 @@ class User_info(models.Model):
         return self.Fullname
 
 class Personlization(models.Model):
-    Portfolio = models.OneToOneField("portfolio.Portfolio", verbose_name=_("User_info"), on_delete=models.RESTRICT, null=True)
+    Portfolio = models.OneToOneField("portfolio.Portfolio", verbose_name=_("User_info"), on_delete=models.CASCADE, null=True)
     CHOICES = (
         ('YELLOW', 'YELLOW'),
         ('RED', 'RED'),
@@ -35,7 +35,7 @@ class Personlization(models.Model):
         ('ORANGE', 'ORANGE')
     )
 
-    Color_theme = models.CharField(_("Color Theme for Portfolio Colors"),choices=CHOICES, max_length=50)
+    Color_theme = models.CharField(_("Color Theme for Portfolio Colors"),choices=CHOICES, max_length=50, default='Choose color theme', help_text='Choose the portfolio color scheme')
     class Meta:
         verbose_name = ("Personalization")
         verbose_name_plural = ("Personalization")
