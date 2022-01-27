b54a6cfdf607961f2d67a71b9c4c1403fe56df65
thoth <thoth2357@gmail.com>
2022-01-27 08:32:16 -0500

    set up social info, personalization and user info models

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -3,31 +3,20 @@ from .models import *
 from django import forms
 
 # Register your models here.
-class User_info_Form(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['Fullname'].required = True
 
-    class Meta:
-        model = User_info
-        fields = '__all__'
 class User_info_inline(admin.StackedInline):
-    form  = User_info_Form
     model = User_info
 
 class Personalization_Inline(admin.TabularInline):
     model = Personlization
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+class Social_info_inline(admin.StackedInline):
+    model = Social_info
 
 @admin.register(Portfolio)
 class PortfolioAdmin(admin.ModelAdmin):
-    inlines = [Personalization_Inline, User_info_inline]
+    inlines = [Personalization_Inline, User_info_inline, Social_info_inline]
 
     readonly_fields = ['Portfolio_name']
-
-
-
-
-
-# admin.site.register(Portfolio)
-# admin.site.register(User_info)
-# admin.site.register(Personlization)
--- portfolio/models.py
+++ portfolio/models.py
@@ -12,7 +12,7 @@ class Portfolio(models.Model):
     # Personlization = models.OneToOneField("portfolio.Personlization", verbose_name=_("personalization"), on_delete=models.CASCADE)
 
     def __str__(self) -> str:
-        return f'Portfolio'
+        return f'Portfolio Config'
 class User_info(models.Model):
     Portfolio = models.OneToOneField("portfolio.Portfolio", verbose_name=_("User_info"), on_delete=models.CASCADE, null=True)
     Fullname = models.CharField(_("FullName"), max_length=50, help_text='Full name to be displayed on portfolio', default='Enter Full Name')
@@ -36,12 +36,30 @@ class Personlization(models.Model):
     )
 
     Color_theme = models.CharField(_("Color Theme for Portfolio Colors"),choices=CHOICES, max_length=50, default='Choose color theme', help_text='Choose the portfolio color scheme')
+    Terminal_icon_blink = models.BooleanField(_("ON or OFF blink terminal icon on homepage"), default=True, null=True)
+    Animation = models.BooleanField(_("ON or OFF animation on homepage"), default=True, null=True)
+
     class Meta:
         verbose_name = ("Personalization")
         verbose_name_plural = ("Personalization")
 
     def __str__(self):
         return self.Color_theme
+
+class Social_info(models.Model):
+    Portfolio = models.OneToOneField("portfolio.Portfolio", verbose_name=_("User_info"), on_delete=models.CASCADE, null=True)
+    Facebook_url = models.URLField(_("Facebook Account Url"), max_length=200, default='None')
+    Instagram_url = models.URLField(_("Instagram Account Url"), max_length=200, default='None')
+    Twitter_url = models.URLField(_("Twitter Account Url"), max_length=200, default='None')
+    LinkedIn_url = models.URLField(_("LinkedIn Account Url"), max_length=200, default='None')
+    EMail_account_name = models.EmailField(_("Email Name"), max_length=254, default='None')
+    
+    class Meta:
+        verbose_name = _("Social_info")
+        verbose_name_plural = _("Social_infos")
+
+    def __str__(self):
+        return f'Social Info'
 
     
 
