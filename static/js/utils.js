0a1fc71c3ae1c9ff4fce1c9430ac1b726576eaa3
thoth <thoth2357@gmail.com>
2022-01-27 08:32:17 -0500

    configured switch to control blinking terminal

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,11 +1,18 @@
 // ** start code blocks used for setting terminal icon to blink on home section ** 
 var icon = document.getElementById('terminal_blink');
+var icon_switch = icon.getAttribute('name');
 
 var interval = window.setInterval(function(){
-    if(icon.style.visibility == 'hidden'){
+    if (icon_switch == 'True'){
+        if(icon.style.visibility == 'hidden'){
+            icon.style.visibility = 'visible';
+        }else{
+            icon.style.visibility = 'hidden';
+        }
+    } 
+    else{
         icon.style.visibility = 'visible';
-    }else{
-        icon.style.visibility = 'hidden';
     }
+    
 }, 300);
 // ** End code blocks used for setting terminal icon to blink on home section ** 
