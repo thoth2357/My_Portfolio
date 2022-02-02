f056ac5f714de516ca30e115a868bbacc479c616
thoth <thoth2357@gmail.com>
2022-02-02 04:31:48 +0100

    experience page done

--- portfolio/views.py
+++ portfolio/views.py
@@ -20,7 +20,7 @@ def home_view(request):
     services_info = Services.objects.all().filter(Portfolio__Portfolio_name="MY PORTFOLIO CONFIG")
     experience_info = Experience.objects.first()
     experience_info_work = Work.objects.all().filter(Experience__User_story=experience_info.User_story)
-
+    experience_info_education = Education.objects.all().filter(Experience__User_story=experience_info.User_story)
     
     profession = [i.strip() for i in user_info.profession.split(',')]
     
@@ -29,8 +29,8 @@ def home_view(request):
     'personlization_info':personlization_info,
     'services_info':services_info,
     'experience_info':experience_info,
-    'experience_info_work':experience_info_work
-
+    'experience_info_work':experience_info_work,
+    'experience_info_education':experience_info_education
     }
     return render(request, "Homepages/index.html", context)
 
